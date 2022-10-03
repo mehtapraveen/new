cd0e3dd562e8e1b6500390b2a0a59aed4db21706
mehtapraveen <praveenmehta503@gmail.com>
2022-10-03 16:30:45 +0530

    hello

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ComponentInteractionComponent } from './component-interaction/component-interaction.component';
+import { BindingsComponent } from './bindings/bindings.component';
+import { FormsModule } from '@angular/forms';
+import { DirectivesComponent } from './directives/directives.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import { CourseListComponent } from './course-list/course-list.component';
+import { SarviceService } from './sarvice.service';
+import{HttpClientModule} from '@angular/common/http';
+import { PageNotfoundComponent } from './page-notfound/page-notfound.component';
+import { CourseDetailsComponent } from './course-details/course-details.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   
+    ComponentInteractionComponent,
+        BindingsComponent,
+        DirectivesComponent,
+        StudentListComponent,
+        CourseListComponent,
+        PageNotfoundComponent,
+        CourseDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [SarviceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
